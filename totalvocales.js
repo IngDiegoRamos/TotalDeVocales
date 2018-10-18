ebe502d3d807ee0b1e882626a83b78d3d5a50c6a
Diego Ramos <31499304+IngDiegoRamos@users.noreply.github.com>
2018-10-17 21:29:35 -0500

    totaldevocales
    totaldevocales

---
+++ totalvocales.js
@@ -0,0 +1,38 @@
+var casos="zrwfhfts joafgbfuquebida ghjehgiydjnndhj kjevrjzhmipp qvse";
+var Cont= 0; 
+document.write("<br>Total de vocales:<br>");
+		for (var i = 0 ; i < casos.length ; i++) {
+
+			switch(casos.charAt(i)) {
+
+				case "a":
+				Cont++;
+				break;
+
+				case "e":
+				Cont++;
+				break;
+
+				case "i":
+				Cont++;
+				break;
+
+				case "o":
+				Cont++;
+				break;
+
+				case "u":
+				Cont++;
+				break;
+
+			}
+		}
+	document.write(Cont + " ");
+
+
+
+
+
+
+
+ 
